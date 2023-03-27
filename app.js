d6f3623fca137892ef1aad79ba0786617d292d70
Mustafa Koc <34mustafakoc@gmail.com>
2023-03-28 00:22:23 +0300

    add public, views folders

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from "express"
 const app = express();
 const port = 3000;
 
+
+//ejs template
+app.set("view engine", "ejs");
+
+//static dosyaların yolunu belirten kod
+app.use(express.static("public"));
+
 app.get("/", (req,res)=>{
-    res.send("test!!...")
+    res.render("index")
+});
+app.get("/about", (req,res)=>{
+    res.render("about")
 });
 
 
